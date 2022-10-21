ee70f280119f30471d9345b5dff3634bbe876793
Michael Henderson <mdhender@mdhender.com>
2022-10-21 17:54:26 -0600

    updated mapgen with tests and fixed linux compile error
    Changed `io.h` to `fcntl.h`. Seems to work on Windows, Mac,
    and Linux.
    
    TODO: There is still a bug with the ilist. If we change from `my_malloc` to `malloc`, the ilist test will segfault.

---
+++ mapgen/prng_test.h
@@ -0,0 +1,11 @@
+// olytag - Olympia: The Age of Gods
+//
+// Copyright (c) 2022 by the OlyTag authors.
+// Please see the LICENSE file in the root directory of this repository for further information.
+
+#ifndef OLYTAG_PRNG_TEST_H
+#define OLYTAG_PRNG_TEST_H
+
+int test_prng(void);
+
+#endif //OLYTAG_PRNG_TEST_H
